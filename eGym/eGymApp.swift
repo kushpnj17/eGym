4378bb5c5df880dd5e78be07ba53f36192b5dcef
Kush Patel <kushpnj17@gmail.com>
2025-10-22 01:40:34 -0400

    Initial Commit

---
+++ eGym/eGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  eGymApp.swift
+//  eGym
+//
+//  Created by Kush Patel on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct eGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eGymTests/eGymTests.swift
@@ -0,0 +1,17 @@
+//
+//  eGymTests.swift
+//  eGymTests
+//
+//  Created by Kush Patel on 10/22/25.
+//
+
+import Testing
+@testable import eGym
+
+struct eGymTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
